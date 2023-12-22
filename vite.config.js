9eb3a61a3fbd08b4f5ef96ad6b1dcd7ba553e2ce
Diah Junaidi <j.fdiah@gmail.com>
2023-12-23 00:36:49 +0700

    config vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Specify the output directory
+  },
   test:{
     environment: "jsdom",
     setupFiles: ['./tests/setup.ts'],
